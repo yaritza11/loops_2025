fabf46006e5308618162f3c268f89f3dcab6ba5c
yaritza11 <yortiz21@cps.edu>
2025-05-06 12:01:03 -0500

    worked on loops

--- 1_loops.py
+++ 1_loops.py
@@ -11,8 +11,17 @@ subjects = ["Math", "Science", "History", "Art"]
 
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
-# Example output: "Subject 0: Math"
+#print(fruits[0])
+#print(fruits[1])
+#print(fruits[2])
 
+for fruit in fruits:
+    print(fruit)
+
+# Example output: "Subject 0: Math"
+subjects = ["Math", "Science", "History", "Art"]
+for subject in subjects:
+    print(subject)
 
 # Given:
 numbers = [5, 10, 15, 20]
